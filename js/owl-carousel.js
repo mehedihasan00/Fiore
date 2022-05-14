20e14ed8a9efb4ba3d5a3a8ac81204d892923cb0
Mehedi Hasan <67319547+mehedihasan00@users.noreply.github.com>
2022-05-14 18:11:16 +0600

    14/06/22

--- js/owl-carousel.js
+++ js/owl-carousel.js
@@ -3,10 +3,12 @@ $(document).ready(function(){
         loop: false,
         margin: 10,
         nav: true,
+        dots: false,
         navText: [
           "<i class='fa-solid fa-chevron-left'></i>",
           "<i class='fa-solid fa-chevron-right'></i>"
         ],
+        // navContainer: '.product-slider .custom-nav',
         autoplay: false,
         autoplayHoverPause: true,
         responsive: {
